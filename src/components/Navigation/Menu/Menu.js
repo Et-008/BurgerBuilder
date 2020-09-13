63323bc89ced8d961d4f7dac98edafbb12c7d817
Et-008 <61292452+Et-008@users.noreply.github.com>
2020-09-14 01:39:53 +0530

    UI added

---
+++ src/components/Navigation/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Menu.css";
+
+let menu = (props) => (
+  <div onClick={props.clicked} className="Menu">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default menu;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavItems from "../NavItems/NavItems";
+import MenuButton from "../Menu/Menu";
 import "./Toolbar.css";
 
 let toolbar = (props) => (
   <header className="Toolbar">
-    <div onClick={props.openMenu} className="ToolbarLogo">
+    <div className="ResponsiveMenuBtn">
+      <MenuButton clicked={props.openMenu} />
+    </div>
+    <div className="ToolbarLogo">
       <Logo />
     </div>
-    <nav className="Responsive">
+    <nav className="ResponsiveNav">
       <NavItems />
     </nav>
   </header>
